0483a3571632938e2e8bf97449eacbfc4530819d
MandE <dabiri1377@gmail.com>
2017-12-01 22:17:53 +0330

    fix "get_maze_from_user" and "find_start_point" def but not test it

--- pojc_stack.py
+++ pojc_stack.py
@@ -1,4 +1,49 @@
-# def get_maze_from_user():
+def get_maze_from_user(x_of_maze_for_get_maze):
+    """
+    get maze from user and return it
+    :return:
+     maze matrix 
+    """
+
+    # guide user
+    print("now enter colma of maze then perss enter \n")
+    print("for wall enter 1")
+    print("for exit point enter $")
+    print("for empty way enter 0")
+    print("for start point enter *")
+    # get maze from user
+    temp_maze = [[j for j in input().split()] for _ in range(x_of_maze_for_get_maze)]
+
+    # TODO: put something hear to check the maze
+
+    return temp_maze
+
+
+def find_start_point(temp_maze, x, y):
+    # x of start point
+    start_point_x = int(0)
+    # y of start point
+    start_point_y = int(0)
+    # flag for check for more than a start point
+    flag_for_start_point = 0
+    # searche for start point in matrix
+    for i_1 in range(x):
+        for j_1 in range(y):
+            if temp_maze[i_1][j_1] == '*':
+                if flag_for_start_point == 0:
+                    # for debug
+                    print("start point = ", end='')
+                    print("x= " + str(i_1) + "y= " + str(j_1))
+                    start_point_x = i_1
+                    start_point_y = j_1
+                    flag_for_start_point = 1
+                else:
+                    print("Err: more than 1 start point")
+
+    place_of_maze_solver = [start_point_x, start_point_y]
+    return place_of_maze_solver
+
+
 def check_udlr(charec, map):
     """
     
@@ -84,50 +129,20 @@ def check_one_side(charec, map, up, left):
 
 # ################### main ################### #
 
-#### get maze
 # the first line of input is the number of rows of the array
-row_of_maze = int(input("Enter numer of row(s): "))
-col_of_maze = int(input("Enter numer of col(s): "))
-# guide user
-print("now enter colma of maze then perss enter \n")
-print("for wall enter 1")
-print("for exit point enter $")
-print("for empty way enter 0")
-print("for start point enter *")
-# get maze from user
+x_of_maze = int(input("Enter number of x(s),row(s): "))
+y_of_maze = int(input("Enter number of y(s),col(s): "))
 
-temp_maze = [[j for j in input().split()] for i in range(row_of_maze)]
-
-# TODO: put something hear to chake the maze
+main_maze = get_maze_from_user()
 
 # show maze
-for i_1 in range(row_of_maze):
-    for j_1 in range(col_of_maze):
-        print(temp_maze[i_1][j_1], end=' ')
+for i_1 in range(x_of_maze):
+    for j_1 in range(y_of_maze):
+        print(main_maze[i_1][j_1], end=' ')
     print("")
 
-##### find start point
-# x of start point
-start_point_x = int(0)
-# y of start point
-start_point_y = int(0)
-# flag for chake for more than a start point
-flag_for_start_point = 0
-# searche for start point in matrix
-for i_1 in range(row_of_maze):
-    for j_1 in range(col_of_maze):
-        if temp_maze[i_1][j_1] == '*':
-            if flag_for_start_point == 0:
-                # for debug
-                print("start point = ", end='')
-                print("x= " + str(i_1) + "y= " + str(j_1))
-                start_point_x = i_1
-                start_point_y = j_1
-                flag_for_start_point = 1
-            else:
-                print("Err: more than 1 start point")
-
 # [x,y] for maze solver
-place_of_solver = [start_point_x, start_point_y]
+place_of_solver = find_start_point(main_maze, x_of_maze, y_of_maze)
+
 # stack , global
 stack_list = []
